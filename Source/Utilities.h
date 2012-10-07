60988fd4d643cd86911ac2266153cdfe88ad4d06
Deepkanwal Plaha <deepkanwal@gmail.com>
2012-10-07 01:10:37 -0400

    Some formatting.

--- Source/Utilities.h
+++ Source/Utilities.h
@@ -8,14 +8,14 @@ class CollisionGrid;
 class GameData;
 
 // Constants
-#define FPS_CAP						60
+#define FPS_CAP					60
 #define WINDOW_WIDTH				600
 #define WINDOW_HEIGHT				600
 #define MAX_WORLD_WIDTH				2000
 #define MAX_WORLD_HEIGHT			2000
 #define STATIC_SCREEN_FILE			"data//static_screen.png"
 #define STATIC_SCREEN_WIDTH			580
-#define STATIC_SCREEN_HEIGHT		80
+#define STATIC_SCREEN_HEIGHT			80
 #define STATIC_SCREEN_X				( WINDOW_WIDTH - STATIC_SCREEN_WIDTH ) / 2
 #define STATIC_SCREEN_Y				( WINDOW_HEIGHT - STATIC_SCREEN_HEIGHT - 10 )
 #define WINDOW_TITLE				"f l i p ' d"
@@ -23,11 +23,11 @@ class GameData;
 #define SURFACE_ENTRIES				3
 #define AUDIO_ENTRIES				3
 
-#define MAPFILE						"data//mapfile.txt"
+#define MAPFILE					"data//mapfile.txt"
 #define MAP_COIN_FILE				"data//map_coin.txt"
-#define MAP_ROWS					12
+#define MAP_ROWS				12
 #define MAP_ROWS_MAX				MAP_ROWS + 12
-#define MAP_COLS					15
+#define MAP_COLS				15
 #define MAP_COLS_MAX				MAP_COLS + 12
 #define PIX_PER_TILE				50
 #define MAP_REVERSED_W				22
@@ -36,11 +36,11 @@ class GameData;
 
 #define MAP_REVERSED_DESTX			150
 #define MAP_COINS_DESTX				500
-#define MAP_COINS_H					25
-#define MAP_COINS_W					19	
-#define MAP_INFO_Y					30
+#define MAP_COINS_H				25
+#define MAP_COINS_W				19	
+#define MAP_INFO_Y				30
 
-#define MAX_COINS					9
+#define MAX_COINS				9
 
 #define PLAYER_IMAGE				"data//player.png"
 #define PLAYER_WIDTH				45		// Should be atleast 4 less than TILE (unless using smaller tiles)
@@ -49,12 +49,12 @@ class GameData;
 #define PLAYER_MAXSPEED_Y			400
 #define PLAYER_GRAVITY				300
 #define PLAYER_JUMP_TIME			350
-#define PLAYER_JUMP_DECREASE		0.80
+#define PLAYER_JUMP_DECREASE			0.80
 
-#define PLAYER_ANIMATION_DELAY		100
-#define PLAYER_ANIMATION_FRAMES		4
+#define PLAYER_ANIMATION_DELAY			100
+#define PLAYER_ANIMATION_FRAMES			4
 
-#define PLAYER_THRESHOLD_JUMP		250
+#define PLAYER_THRESHOLD_JUMP			250
 
 #define SW_HIDE 0
 
